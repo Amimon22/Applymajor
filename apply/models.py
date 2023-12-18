b8e816d1a3a90c2bf6a1be0b4c2bea1f1e41f694
Yeryeong0101 <fud030101@gmail.com>
2023-12-18 23:27:24 +0900

    모의지원 모델 -예령

--- apply/models.py
+++ apply/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Apply(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)   #유저 불러오기
+    department = models.CharField(max_length = 200)   #소속대학(과기대, 글융, 아앤디)
+    orderOfApplication1 = models.CharField(max_length = 200)   #1지망
+    orderOfApplication2 = models.CharField(max_length = 200)   #2지망
+    orderOfApplication3 = models.CharField(max_length = 200)   #3지망
+    orderOfApplication4 = models.CharField(max_length = 200)   #4지망
