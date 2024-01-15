55353d75241ca05d3111c26af7845da357cb88c0
Yeryeong0101 <fud030101@gmail.com>
2024-01-16 00:51:10 +0900

    지망 분리 후 경쟁률 계산 + 이전 선택전공 저장

--- apply/admin.py
+++ apply/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Source, Academic, Course, Department, Subject_code, Major, Grade, Choice, User_apply_profile
+from .models import Source, Academic, Course, Department, Subject_code, Major, Grade, Choice, User_apply_profile, UserPriorChoice
 
 admin.site.register(Source)
 admin.site.register(Academic)
@@ -10,3 +10,4 @@ admin.site.register(Subject_code)
 admin.site.register(Major)
 admin.site.register(Choice)
 admin.site.register(User_apply_profile)
+admin.site.register(UserPriorChoice)
