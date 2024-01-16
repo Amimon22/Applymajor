50cd876cfc60b407b5091ad390e87e7b3777b824
seojinBaek <baekseojin723@gmail.com>
2024-01-16 19:03:23 +0900

    0116 로그인 성공시 마이페이지로 이동 수정

--- user/urls.py
+++ user/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from . import views
 from .views import user, register, user_logout, logout_success,change_password, mypage, mypage_view
 
 
 urlpatterns = [
     path('login/', user, name='login'),  # 로그인 페이지 URL 
-    path('login/success/', views.login_success, name='success'),
+    #path('login/success/', views.login_success, name='success'),
     path('register/', register, name='register'),  # 회원가입 페이지 URL 
     path('accounts/change_password/', change_password, name='change_password'),  #비밀번호 재설정 페이지 URL 
     path('logout/', user_logout, name='logout'),
